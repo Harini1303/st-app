0f9fa8fe79de5c2bba8158f2c33d29ca998527cd
Harini1303 <117617638+Harini1303@users.noreply.github.com>
2024-01-09 16:00:38 +0530

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,23 @@
+import streamlit as st 
+
+st.header('“YOUR PROGRESS IS OUR MISSION..!”')
+st.markdown('<style>div.block-container{padding-top:2rem}</style>',unsafe_allow_html=True)
+st.markdown('''<style>
+            .small-font{font-size:22px;margin-top:-22px}
+            </style>''',unsafe_allow_html=True)
+st.markdown('<p class="small-font">Welcome to Stoxila – Your Ultimate Destination for Intra-Day Trading Excellence!</p>',unsafe_allow_html=True)
+st.write("WHY STOXILA?")
+st.markdown("- EXPERTISE REDEFINED")
+st.markdown("- ASSISTANCE AT YOUR FINGERTIPS")
+st.markdown("- EFFICIENCY UNLEASHED ")
+st.markdown("- INTEGRITY AS STANDARD")
+st.markdown("- ALGORITHMIC BRILLIANCE")
+st.markdown("- DOMAIN MASTERY")
+
+st.markdown('''
+<style>
+[data-testid="stMarkdownContainer"] ul{
+    list-style-position: inside;
+}
+</style>
+''', unsafe_allow_html=True)
